46e2b956e64b7938ffcf417270d7ca5f16bf58bd
Adrian Reyer <are@lihas.de>
2015-12-22 11:58:20 +0100

    bugfix, missing (

--- mysql-replication-fix-lvm-drbd-ha-vserver.sh
+++ mysql-replication-fix-lvm-drbd-ha-vserver.sh
@@ -234,7 +234,7 @@ if [ x$DRBD == x1 ]; then
 	ssh -X -a $SEITEB "(
 	  mount -t $FS /dev/$VGSEITEB/tempmysqlsnap /mnt/tempmysqlsnap &&
 	  ( if [ x$NOTMPLV == x0 ]; then
-	      cd /mnt/tempmysqlsnap/$RELPATH/ && tar cf - . ) |
+	      ( cd /mnt/tempmysqlsnap/$RELPATH/ && tar cf - . ) |
 	      mbuffer -m 8G |
 	      ( cd /mnt/tempmysqlsync && tar xvpf - ) &&
 	      umount /mnt/tempmysqlsnap &&
